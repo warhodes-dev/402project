f95497be7cc230a3149d8f07367296b4ecb94508
warhodes-dev <warhodes@gmail.com>
2022-04-25 14:50:29 -0400

    Add files via upload

---
+++ ff7k.sh
@@ -0,0 +1,11 @@
+ffmpeg \
+-f v4l2 -i /dev/video0 \
+-c:v h264_nvmpi -preset:v ultrafast -profile:v high -level:v 4.0 \
+-b:v 7000k \
+-fflags nobuffer \
+-f flv rtmp://73.108.220.91/live/jetson
+
+# Extra options:
+#-pix_fmt yuv420p 
+#-b:v 10000k \
+#-video_size 2304x1536
